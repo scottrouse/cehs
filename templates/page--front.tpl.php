76c73292212b3e89b88efc42885674622e4ae881
Scott Rouse <scottwrouse@gmail.com>
2011-11-12 17:20:23 -0700

    who knows when i last committed. This is a commit from the live site

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -166,7 +166,6 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content_top']); ?>
-      <?php print render($page['content']); ?>
       <?php print render($page['content_bottom']); ?>
       <?php print $feed_icons; ?>
     </div></div><!-- /.section, /#content -->
